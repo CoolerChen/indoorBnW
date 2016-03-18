29c03f77a3c5b6336da0f71898898bbb5c0bdc92
CoolerChen <leo@LEO-MacBook-Pro.local>
2016-03-18 14:31:39 +0800

    add empty BrowseMessageLocal.swift

---
+++ indoorBnW/BrowseMessageLocal.swift
@@ -0,0 +1,36 @@
+//
+//  BrowseMessageLocal.swift
+//  indoorBnW
+//
+//  Created by LEO on 2016/3/18.
+//  Copyright © 2016年 LEO. All rights reserved.
+//
+
+import UIKit
+
+class BrowseMessageLocal: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let gesture = UITapGestureRecognizer(target: self, action: "someAction:")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
