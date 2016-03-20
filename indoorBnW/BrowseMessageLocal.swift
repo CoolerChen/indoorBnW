22f67bbdd4f6b3f4c45d0e8c8e53513a0d324928
CoolerChen <leo@LEO-MacBook-Pro.local>
2016-03-20 22:07:41 +0800

    user瀏覽訊息update

--- indoorBnW/BrowseMessageLocal.swift
+++ indoorBnW/BrowseMessageLocal.swift
@@ -15,20 +15,13 @@ class BrowseMessageLocal: UIViewController, UIScrollViewDelegate {
 //    var m_tableView:UITableView!
     var m_scrollerView:UIScrollView!
     var m_pageControl:UIPageControl!
-    var m_messageIndex:Int = 0
+    var m_messageIndex:Int = 0 //第幾項，從1開始
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.view.backgroundColor = UIColor(red: 209/255.0, green: 235/255.0, blue: 254/255.0, alpha: 1)
         self.automaticallyAdjustsScrollViewInsets = false
-        
-//        m_tableView = UITableView(frame: CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height))
-//        m_tableView.backgroundColor = UIColor.clearColor()
-//        m_tableView.autoresizingMask = [UIViewAutoresizing.FlexibleHeight, UIViewAutoresizing.FlexibleWidth]
-//        m_tableView.delegate = self
-//        m_tableView.dataSource = self
-//        self.view.addSubview(m_tableView)
         
         loadMessageLocalData()
         refreshWithFrame(self.view.frame)
@@ -51,7 +44,6 @@ class BrowseMessageLocal: UIViewController, UIScrollViewDelegate {
     }
     
     func loadMessageLocalData() {
-        let db = SQLiteDB.sharedInstance()
         data = db.query("select * from messagelocal where 1=1")
         /*
             id             : 1
@@ -64,122 +56,6 @@ class BrowseMessageLocal: UIViewController, UIScrollViewDelegate {
             addFavorite    :加入收藏
         */
     }
-
-//    //MARK: - TableView
-//    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return data!.count
-//    }
-//    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//        return 1
-//    }
-//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-//    {
-//        //詢問是否有可以覆用的cell   問有沒有kDisplayCell_ID這個標籤的cell可以用
-//        var cell:UITableViewCell? = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell!
-//        if cell == nil { //剛生成畫面的第一次都是nil  reuseIdentifier重複使用標籤
-//            cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
-//            cell!.selectionStyle = UITableViewCellSelectionStyle.Blue //點選後改變的顏色
-//            //cell!.showsReorderControl = true  //是否可以排序
-//        }
-////        cell?.textLabel?.text = data![indexPath.row].objectForKey("messageTitle") as? String //主標題
-////        cell?.detailTextLabel?.text = data![indexPath.row].objectForKey("messageSubtitle") as? String //副標題
-////        cell?.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator//右邊的
-//        
-//        let viewW = (cell?.frame.width)!
-//        let viewH = (cell?.frame.height)!
-//        
-////        let viewStartY:CGFloat = 0
-//        var usedY:CGFloat = 10
-////
-////        let scrollView = UIView(frame: CGRectMake(
-////            ((cell?.frame.width)! - (cell?.frame.width)!*0.9)/2, //x
-////            5, //(( viewH-viewStartY ) - ( viewH-viewStartY )*0.85)/2 + viewStartY, //y
-////            (cell?.frame.width)!*0.9, //w
-////            ( (cell?.frame.height)!-viewStartY )*0.85)) //h
-////        scrollView.backgroundColor = UIColor(red: 0.85, green: 0.85, blue: 0.58, alpha: 1)
-////        scrollView.layer.cornerRadius = 10
-////        scrollView.layer.masksToBounds = true
-////        cell?.addSubview(scrollView)
-//        usedY = 20 //scrollView.frame.origin.y + 20
-//        
-////        let viewW = scrollView.frame.width
-////        let viewH = scrollView.frame.height
-//        
-//        //標題
-//        let titleLabel = UILabel(frame: CGRectMake(
-//            (viewW - (viewW)*0.9)/2, //x
-//            usedY, //y
-//            (viewW)*0.9, 35)) //w, h
-//        titleLabel.backgroundColor = UIColor.lightGrayColor()
-//        titleLabel.text = data![0].objectForKey("messageTitle") as? String
-//print("標題: \(data![0].objectForKey("messageTitle") as? String)")
-//        cell?.addSubview(titleLabel)
-//        usedY += titleLabel.frame.height + 20
-//        
-//        //副標題
-//        let subtitleLabel = UILabel(frame: CGRectMake(
-//            titleLabel.frame.origin.x, //x
-//            usedY, //y
-//            (viewW)*0.9, 35)) //w, h
-//        subtitleLabel.backgroundColor = UIColor.lightGrayColor()
-//        subtitleLabel.text = data![0].objectForKey("messageSubtitle") as? String
-//        cell?.addSubview(subtitleLabel)
-//        usedY += subtitleLabel.frame.height + 20
-//        
-//        //內容
-//        let contentTextView = UITextView(frame: CGRectMake(
-//            titleLabel.frame.origin.x, //x
-//            usedY, //y
-//            (viewW)*0.9, 70)) //w, h
-//        contentTextView.backgroundColor = UIColor.lightGrayColor()
-//        contentTextView.font = UIFont(name: ".SFUIText-Regular", size: 17) //改變textview字型大小
-//        contentTextView.text = data![0].objectForKey("messageContent") as? String
-//        contentTextView.selectable = false
-//        cell?.addSubview(contentTextView)
-//        usedY += contentTextView.frame.height + 20
-//        
-//        //圖片
-//        let imageImageView = UIImageView(frame: CGRectMake(
-//            ((viewW) - (viewW)*0.9)/2, //x
-//            usedY, //y
-//            (viewW)*0.9, 200)) //w, h
-//        imageImageView.backgroundColor = UIColor.lightGrayColor()
-//        imageImageView.userInteractionEnabled = false
-//        
-////        let imageName = data![0].objectForKey("messageImage") as? String
-////        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {() -> Void in
-////            let tempData = NSData(contentsOfURL: NSURL(string: "http://bing0112.100hub.net/bing/MessageImage/Sample.jpg")!)
-////            
-////            dispatch_async(dispatch_get_main_queue(),{
-////                if tempData != nil {
-////                    imageImageView.image = UIImage(data: tempData!)
-////                } else {
-////                    imageImageView.backgroundColor = UIColor.redColor()
-////                }
-////            })
-////        })
-////        scrollView.addSubview(imageImageView)
-//        
-////        let imageName = data![0].objectForKey("messageImage") as? String
-////        let tempData = NSData(contentsOfURL: NSURL(string: "http://bing0112.100hub.net/bing/MessageImage/\(imageName!)")!)
-////        if tempData != nil {
-////            imageImageView.image = UIImage(data: tempData!)
-////        } else {
-////            imageImageView.backgroundColor = UIColor.redColor()
-////        }
-////        cell?.addSubview(imageImageView)
-//        usedY += imageImageView.frame.height + 20
-//        
-//        //ScrollView ContentSize
-////        scrollView.contentSize = CGSizeMake(scrollView.frame.width, usedY)
-//        
-//        cell?.selectionStyle = .None
-//        
-//        return cell!
-//    }
-//    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-//        return (self.view.frame.height - 64)
-//    }
 
     func refreshWithFrame(frame:CGRect) {
         m_scrollerView = UIScrollView() //宣告空的ScrollView
@@ -226,16 +102,26 @@ class BrowseMessageLocal: UIViewController, UIScrollViewDelegate {
         let corRadius:CGFloat = 5
         let interval:CGFloat = 10
         
+        let scrollView = UIScrollView(frame: CGRectMake(
+            (pView.frame.width - pView.frame.width*0.9)/2, //x
+            (pView.frame.height - pView.frame.height*0.9)/2 + 10, //y
+            pView.frame.width*0.9, //w
+            pView.frame.height*0.9)) //h
+        scrollView.backgroundColor = UIColor(red: 209/255.0, green: 235/255.0, blue: 254/255.0, alpha: 1)
+        scrollView.layer.cornerRadius = corRadius*2
+        scrollView.layer.masksToBounds = true
+        pView.addSubview(scrollView)
+        
         //商店
         let storeLabel = UILabel(frame: CGRectMake(
-            (pView.frame.width - pView.frame.width*0.9)/2, //x
-            interval, //y
-            pView.frame.width*0.9, 35)) //w, h
+            (scrollView.frame.width - scrollView.frame.width*0.9)/2, //x
+            20, //y
+            scrollView.frame.width*0.9, 35)) //w, h
         storeLabel.backgroundColor = UIColor.lightGrayColor()
         storeLabel.layer.cornerRadius = corRadius
         storeLabel.layer.masksToBounds = true
-        if data?.count > 0 {storeLabel.text = data![index].objectForKey("messageStore") as? String}
-        pView.addSubview(storeLabel)
+        if data?.count > 0 {storeLabel.text = data![index]["messageStore"] as? String}
+        scrollView.addSubview(storeLabel)
         usedHeight += storeLabel.frame.origin.y + storeLabel.frame.height + interval
         
         let alignX = (pView.frame.width - pView.frame.width*0.9)/2
@@ -244,12 +130,12 @@ class BrowseMessageLocal: UIViewController, UIScrollViewDelegate {
         let titleLabel = UILabel(frame: CGRectMake(
             alignX, //x
             usedHeight, //y
-            pView.frame.width*0.9, 35)) //w, h
+            scrollView.frame.width*0.9, 35)) //w, h
         titleLabel.backgroundColor = UIColor.lightGrayColor()
         titleLabel.layer.cornerRadius = corRadius
         titleLabel.layer.masksToBounds = true
         if data?.count > 0 {titleLabel.text = data![index].objectForKey("messageTitle") as? String}
-        pView.addSubview(titleLabel)
+        scrollView.addSubview(titleLabel)
         usedHeight += titleLabel.frame.height + interval
 //        print("2 \(usedHeight)")
         
@@ -257,12 +143,12 @@ class BrowseMessageLocal: UIViewController, UIScrollViewDelegate {
         let subtitleLabel = UILabel(frame: CGRectMake(
             alignX, //x
             usedHeight, //y
-            pView.frame.width*0.9, 35)) //w, h
+            scrollView.frame.width*0.9, 35)) //w, h
         subtitleLabel.backgroundColor = UIColor.lightGrayColor()
         if data?.count > 0 {subtitleLabel.text = data![index].objectForKey("messageSubtitle") as? String}
         subtitleLabel.layer.cornerRadius = corRadius
         subtitleLabel.layer.masksToBounds = true
-        pView.addSubview(subtitleLabel)
+        scrollView.addSubview(subtitleLabel)
         usedHeight += subtitleLabel.frame.height + interval
 //        print("3 \(usedHeight)")
         
@@ -270,14 +156,14 @@ class BrowseMessageLocal: UIViewController, UIScrollViewDelegate {
         let contentTextView = UITextView(frame: CGRectMake(
             alignX, //x
             usedHeight, //y
-            pView.frame.width*0.9, 70)) //w, h
+            scrollView.frame.width*0.9, 70)) //w, h
         contentTextView.backgroundColor = UIColor.lightGrayColor()
         contentTextView.font = UIFont(name: ".SFUIText-Regular", size: 17) //改變textview字型大小
         if data?.count > 0 {contentTextView.text = data![index].objectForKey("messageContent") as? String}
         contentTextView.selectable = false
         contentTextView.layer.cornerRadius = corRadius
         contentTextView.layer.masksToBounds = true
-        pView.addSubview(contentTextView)
+        scrollView.addSubview(contentTextView)
         usedHeight += contentTextView.frame.height + interval
 //        print("4 \(usedHeight)")
         
@@ -285,9 +171,9 @@ class BrowseMessageLocal: UIViewController, UIScrollViewDelegate {
         let imageImageView = UIImageView(frame: CGRectMake(
             alignX, //x
             usedHeight, //y
-            pView.frame.width*0.9, 200)) //w, h
+            scrollView.frame.width*0.9, 200)) //w, h
         imageImageView.userInteractionEnabled = false
-        let imageName = NSHomeDirectory() + "/Documents/images/" + (data![index].objectForKey("messageImage") as? String)!
+        let imageName = NSHomeDirectory() + "/Documents/images/msg/" + (data![index].objectForKey("messageImage") as? String)!
 //        print("\(imageName) ========================...")
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {() -> Void in
             let tempData = NSData(contentsOfFile: imageName)
@@ -300,8 +186,66 @@ class BrowseMessageLocal: UIViewController, UIScrollViewDelegate {
                 }
             })
         })
-        pView.addSubview(imageImageView)
+        scrollView.addSubview(imageImageView)
         usedHeight += imageImageView.frame.height + interval
+        
+        //加入收藏按鈕
+        let buttonH = pView.frame.height*0.08
+        let buttonW = CGFloat(60)
+        let favImage = UIImageView(frame: CGRectMake(
+            pView.frame.width - pView.frame.width*0.05 - buttonW , //x
+            pView.frame.height*0.06 - buttonH/2, //y
+            buttonW, //w
+            buttonH)) //h
+        favImage.backgroundColor = UIColor(red: 209/255.0, green: 235/255.0, blue: 254/255.0, alpha: 1)
+        favImage.layer.cornerRadius = 10
+        favImage.layer.masksToBounds = true
+//        favButton.setImage(UIImage(named: "FavoriteStarO"), forState: UIControlState.Normal)
+        if data?.count > 0 {
+            if (data![index]["addFavorite"] as? String) == "yes" {
+                favImage.image = UIImage(named: "FavoriteStarO") //橘色
+            } else {
+                favImage.image = UIImage(named: "FavoriteStarG") //灰色
+            }
+        }
+        favImage.contentMode = UIViewContentMode.ScaleAspectFit
+//        favButton.clipsToBounds = true
+        favImage.userInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: "imageTapped")
+        favImage.addGestureRecognizer(tapGesture)
+        favImage.tag = index + 1
+//        print("favImage.tag = \(m_messageIndex)")
+        pView.addSubview(favImage)
+    }
+    
+    func imageTapped() {
+//        let index = m_messageIndex - 1
+//        print("m_messageIndex = \(m_messageIndex)")
+        
+        //改變sqlite addFavorite欄位的值
+        let messageTime = data![m_messageIndex - 1].objectForKey("messageTime") as? String
+        let favValue = db.query("select addFavorite from messagelocal where messageTime='\(messageTime!)'")
+        
+        if favValue.count > 0 {
+            let willChangeImage = self.view.viewWithTag(m_messageIndex) as! UIImageView
+//            print(m_scrollerView.viewWithTag(0)?.viewWithTag(m_messageIndex))
+//            print(self.view.viewWithTag(0)?.viewWithTag(m_messageIndex))
+//            print(self.view.viewWithTag(index))
+//            m_scrollerView.subviews.
+            
+            if (favValue[0]["addFavorite"] as? String) == "yes" {
+                let reV = db.query("update messagelocal set addFavorite='no' where messageTime='\(messageTime!)'")
+                willChangeImage.image = UIImage(named: "FavoriteStarG") //灰色
+//                print("index:\(m_messageIndex - 1) set no: \(reV)")
+            } else {
+                let reV = db.query("update messagelocal set addFavorite='yes' where messageTime='\(messageTime!)'")
+                
+                willChangeImage.image = UIImage(named: "FavoriteStarO") //橘色
+//                print("index:\(m_messageIndex - 1) set yes: \(reV)")
+            }
+        } else {
+            print("count 0")
+        }
     }
     
     func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
